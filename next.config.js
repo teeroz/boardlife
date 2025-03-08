f71241d5e9086f3fd9b0c218364ca6a2a6ece65e
teeroz <prophet75@gmail.com>
2025-03-08 23:43:01 +0900

    Next.js 이미지 최적화 설정 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  images: {
+    domains: ['boardlife.co.kr'],
+    unoptimized: true, // 도커 환경에서 이미지 최적화를 비활성화
+  },
 }
 
 module.exports = nextConfig 
